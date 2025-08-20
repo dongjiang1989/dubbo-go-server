50a030f3ecd89b7915620821f013de8a4dcd8394
dongjiang <dongjiang1989@126.com>
2025-08-20 10:40:03 +0800

    update config
    Signed-off-by: dongjiang <dongjiang1989@126.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"dubbo.apache.org/dubbo-go/v3/config"
@@ -28,19 +27,8 @@ func main() {
 	ticker := time.NewTicker(3 * time.Second)
 	defer ticker.Stop()
 
-	done := make(chan bool)
-
-	go func() {
-		var input string
-		fmt.Scanln(&input)
-		done <- true
-	}()
-
 	for {
 		select {
-		case <-done:
-			logger.Infof("stop dubbo client")
-			return
 		case <-ticker.C:
 			req := &node.NodeRequest{}
 			reply, err := grpcNodeImpl.GetNodeInfo(context.Background(), req)
@@ -50,4 +38,6 @@ func main() {
 			logger.Infof("client response result: %v\n", reply)
 		}
 	}
+
+	return
 }
